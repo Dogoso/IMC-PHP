f53c6ae58dc0effcac998236fce94dfd5e5a1634
Silas Lago <juanfsa@live.com>
2022-10-27 15:52:37 -0300

    Showing result in screen

--- calcularIMC.php
+++ calcularIMC.php
@@ -8,11 +8,25 @@
         header('Location: /erro.html');
     }
 
-    $imc = $peso / ($altura * $altura);
+    $imc = intval($peso) / (intval($altura) * intval($altura));
 
     echo '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Sucesso!</title></head><body>';
     print_r('<h1>' . $nome . '</h1>');
     print_r('<p>Altura de: ' . $altura . '</p>');
     print_r('<p>Peso de: ' . $peso . '</p>');
-    print_r('<p>Seu resultado é:'. $imc .'</p>');
+    print_r('<p>Seu resultado é: '. $imc .'</p>');
+
+    $resultado = '';
+    if($imc < 18.5) {
+        $resultado = 'Magreza';
+    } else if($imc < 24.9) {
+        $resultado = 'Normal';
+    } else if($imc < 29.9) {
+        $resultado = 'Sobrepeso';
+    } else if($imc < 39.9) {
+        $resultado = 'Obesidade';
+    } else {
+        $resultado = 'Obesidade Grave';
+    }
+    print_r('<p>Ou seja: '. $resultado .'</p>');
     echo '</body></html>';
